b5a9a0ee1acda1d967f4ca38487392aff62addac
MelvinRabang <mirabang_88@yahoo.com>
2018-11-11 18:04:17 +0800

    Add location parameter and fix api key functionality

--- src/main/java/com/weatherlog/app/controller/WeatherLogApiController.java
+++ src/main/java/com/weatherlog/app/controller/WeatherLogApiController.java
@@ -2,25 +2,38 @@ package com.weatherlog.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/weatherlog")
 public class WeatherLogApiController {
 
 	@Autowired
-	RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 	 
-	 
-	@Value("${operations.restURL}")
-	String serviceURL;
-	 
+	@Value("${openWeatherMapClient.url}")
+	private String clientURL;
+	
+	@Value("${openWeatherMapClient.apiKey}")
+	private String apiKey;
+
+	private static final String LOCATION_CLIENT_PARAM = "?q=";
+	private final String AND_CHAR = "&";
+
 	@GetMapping()
-	String getURLAvailability() {
-		return restTemplate.getForObject(serviceURL, String.class);
+	String getURLAvailability(@RequestParam(name="location") String location) {
+		String finalUrl = extractClientUrl(location);
+		System.out.println("finalUrl" + finalUrl);
+		return restTemplate.getForObject(finalUrl, String.class);
 	}
 	
+	private String extractClientUrl(String location) {
+		return clientURL + LOCATION_CLIENT_PARAM + location  + AND_CHAR + apiKey;
+	}
 }
